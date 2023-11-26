e6d8644dda33ad829358b6f932983af914bb6656
Joshi <joshua@lipps.de>
2023-11-26 15:27:19 +0100

    initialized CustomFields#ans as 0

--- src/main/CustomFields.java
+++ src/main/CustomFields.java
@@ -1,5 +1,5 @@
 package main;
 
 public class CustomFields {
-	public static Object ans; // the last value that's been printed
+	public static Object ans = 0; // the last value that's been printed
 }
